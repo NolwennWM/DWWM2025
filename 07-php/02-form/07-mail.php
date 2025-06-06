001820513b7902f3a0f5a5112d9085cc1c0aad69
Nolwenn <nolwenn.wm@afci-formation.fr>
2025-06-06 10:27:54 +0200

    mailer lesson php

--- 07-php/02-form/07-mail.php
+++ 07-php/02-form/07-mail.php
@@ -1,10 +1,12 @@
 <?php 
 
 /* 
-    Coder un envoi de mail via PHP qui soit sécurisé et sans erreur est très complexe.
-    C'est pour cela que la plupart des gens passent par un bibliothèque comme par exemple "PHPMailer".
-    Pour l'installer nous allons avoir besoin du gestionnaire de packet "composer" qui lui même aura besoin d'une installation de "PHP".
+    PHPで安全かつエラーのないメール送信機能を実装するのは非常に複雑です。
+    そのため、ほとんどの人は「PHPMailer」のようなライブラリを利用します。
+    
+    このライブラリをインストールするには、「composer」というパッケージマネージャーが必要です。
+    そして composer を使うには、PHP自体がインストールされている必要があります。
 
-    Pour installer PHPMailer, on tapera la commande :
+    PHPMailerをインストールするには、以下のコマンドを実行します：
         * ***REMOVED*** phpmailer/phpmailer
 */
