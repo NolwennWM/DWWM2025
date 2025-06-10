6f8b20b26e816cdc5c99ecd5954b18f704ad6a01
Nolwenn <nolwenn.wm@afci-formation.fr>
2025-06-10 13:26:59 +0200

    PHP CRUD lesson connexion and read

--- 07-php/03-crud/00-database.php
+++ 07-php/03-crud/00-database.php
@@ -1,73 +1,73 @@
 <?php 
 /* 
-    En PHP, il existe plusieurs outils pour se connecter à une BDD.
-    On trouvera par exemple "mysqli" ou "pdo"
-    Ici nous verrons "pdo" (PHP Data Object)
-    
-    Pour se connecter, PDO demande un DSN (Data Source Name)
-    C'est un string contenant toute les informations pour localiser la BDD.
-    {pilote}:host={adresse};port={port de connexion};dbname={nom de la bdd};charset={charset à utiliser}
+    In PHP, there are several tools for connecting to a database.
+    Examples include "mysqli" or "pdo."
+    Here, we'll see "pdo" (PHP Data Object).
+
+    To connect, PDO requires a DSN (Data Source Name).
+    This is a string containing all the information needed to locate the database.
+    {driver}:host={address};port={connection port};dbname={database name};charset={charset to use}
 */
 $dsn = "mysql:host=bddsql;port=3306;dbname=biere;charset=utf8mb4";
 /* 
-    Lorsque l'on crée une nouvelle instance de PDO, il prendra les paramètres suivant :
-        1. le DSN
-        2. le nom d'utilisateur pour se connecter à la BDD
-        3. le mot de passe pour la BDD
-        4. un tableau contenant la configuration de PDO
+    When creating a new PDO instance, it will take the following parameters:
+        1. the DSN
+        2. the username to connect to the database
+        3. the database password
+        4. an array containing the PDO configuration
 */
 $pdo = new PDO($dsn, "root", "root", [
-    // Type d'erreur que dois afficher PDO :
+    // Type of error that PDO should display:
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    // Comment les données récupéré en BDD doivent être affiché
+    // How data retrieved from the database should be displayed
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    // Indique à PDO de ne pas émuler les requêtes préparés (voir plus bas)
+    // Tells PDO to not emulate prepared statements (see below)
     // PDO::ATTR_EMULATE_PREPARES => false
 ]);
 
-// Envoyer une requête à la BDD :
+// Send a request to the database:
 $sqlCouleur = $pdo->query("SELECT * FROM couleur");
 // var_dump($sql);
 
-// récupérer les données :
+// recover data :
 $couleurs = $sqlCouleur->fetchAll();
 // var_dump($couleurs);
-// Si on veut récupérer que le premier résultat, on utilisera "fetch"
+// If we want to retrieve only the first result, we will use "fetch"
 $sqlType = $pdo->query("SELECT * FROM type");
 $type = $sqlType->fetch();
 
 /* 
-    Lorsque la requête ne demande aucune entrée utilisateur, "query" est suffisant.
-    Mais si la requête contient des entrées utilisateur, il ne faudra surtout pas utilisé query. Au risque d'avoir des injections SQL.
+    When the query doesn't require any user input, "query" is sufficient.
+    But if the query contains user input, you should definitely not use query. Otherwise, you risk SQL injection.
 
-    Il nous faudra utiliser des requêtes préparés. 
-    Si votre pilote de BDD (mysql, mariadb, postgresql...) ne gère pas les requêtes préparés,
-    PDO peut les émuler. (ici mariadb les gère, l'émulation a donc été désactivé)
+    We'll need to use prepared statements.
+    If your database driver (mysql, mariadb, postgresql, etc.) doesn't support prepared statements,
+    PDO can emulate them. (Here, mariadb does, so emulation has been disabled.)
 
-    On utilisera la méthode "prepare" dans laquelle on viendra écrire la requête sql en remplaçant les entrées utilisateur par :
-        "?" ou ":unMotChoisi"
+    We'll use the "prepare" method, in which we'll write the SQL query, replacing user input with:
+    "?" or ":aChosenWord"
 */
 $sqlCouleur2 = $pdo->prepare("SELECT * FROM couleur WHERE NOM_COULEUR = ?");
 /* 
-    Puis on passera les entrées utilisateur soit par des méthodes vu plus bas, 
-    soit par "execute" auquel on passera un tableau.
-        Si des "?" ont été utilisé, le premier élément du tableau ira au premier "?" et ainsi de suite.
-        Si des ":unMot" ont été utilisé, on donnera un tableau associatif :
-            "SELECT * FROM couleur WHERE NOM_COULEUR = :maSuperCouleur"
-            execute(["maSuperCouleur"=>$entreUtilisateur]);
+    Then we'll pass the user input either through the methods discussed below,
+    or through "execute", which we'll pass an array.
+        If "?" was used, the first element of the array will go to the first "?", and so on.
+        If ":aWord" was used, we'll return an associative array:
+        "SELECT * FROM color WHERE COLOR_NAME = :mySuperColor"
+        execute(["mySuperColor"=>$entryUser]);
 */
 $entreUtilisateur = "Blanche";
 $sqlCouleur2->execute([$entreUtilisateur]);
 $couleurFromPrepare = $sqlCouleur2->fetch();
 /* 
-    Dans le cas d'un execute, tous les paramètres passées, sont traité comme des strings.
-    Ce qui peut poser problème dans certain cas, comme pour indiquer une limite.
-    Une autre façon de donner les paramètres existe :
-        "bindValue" et "bindParam"
-        Ils fonctionnent de la même manière :
-            premier paramètre, le nom dans la requête préparé,
-            Second paramètre,  la valeur à utiliser,
-            Troisième paramètre, une constante de PDO indiquant le type de la valeur
+    In the case of execute, all parameters passed are treated as strings.
+    This can be problematic in certain cases, such as when specifying a limit.
+    Another way to pass parameters exists:
+        "bindValue" and "bindParam"
+        They work the same way:
+        First parameter: the name in the prepared statement,
+        Second parameter: the value to use,
+        Third parameter: a PDO constant indicating the type of the value
                 PDO::PARAM_NULL
                 PDO::PARAM_BOOL
                 PDO::PARAM_INT
@@ -75,42 +75,42 @@ $couleurFromPrepare = $sqlCouleur2->fetch();
 */
 $sqlArticle = $pdo->prepare("SELECT * FROM article LIMIT :lim");
 $sqlArticle->bindValue("lim", 5, PDO::PARAM_INT);
-// dans ce cas là execute reste vide:
+// in this case execute remains empty:
 $sqlArticle->execute();
-// Par exemple, ceci provoquera une erreur, si PDO emule la requête préparé :
+// For example, this will cause an error, if PDO emulates the prepared statement :
 // $sqlArticle->execute(["lim"=>5]);
 $articles = $sqlArticle->fetchAll();
 
-// ? Différence entre bindValue et bindParam :
+// ? Difference between bindValue and bindParam :
 
-// ? Exemple avec bindValue :
-// Je choisi une couleur
+// ? Example with bindValue :
+// I choose a color
 $UserCouleur = "Blanche";
-// Je prépare la requête
+// I am preparing the request
 $sqlBindValue = $pdo->prepare("SELECT * FROM couleur WHERE NOM_COULEUR = :col");
-// Je lance un bindValue
+// I call bindValue
 $sqlBindValue->bindValue("col", $UserCouleur, PDO::PARAM_STR);
-// Je change la couleur
+// I change the color
 $UserCouleur = "Ambrée";
-// J'execute la requête
+// I execute the query
 $sqlBindValue->execute();
 $couleurBindValue = $sqlBindValue->fetch();
 
-// ? Exemple avec bindParam :
-// Je choisi une couleur
+// ? Example with bindParam :
+// I choose a color
 $UserCouleur = "Blanche";
-// Je prépare la requête
+// I am preparing the request
 $sqlBindParam = $pdo->prepare("SELECT * FROM couleur WHERE NOM_COULEUR = :col");
-// Je lance un bindParam
+// I call bindParam
 $sqlBindParam->bindParam("col", $UserCouleur, PDO::PARAM_STR);
-// Je change la couleur
+// I change the color
 $UserCouleur = "Ambrée";
-// J'execute la requête
+// I execute the query
 $sqlBindParam->execute();
 $couleurBindParam = $sqlBindParam->fetch();
 /* 
-    Avec bindValue, la valeur dans la variable est directement passé à notre requête préparé.
-    Avec bindParam, c'est la variable entière qui est passé. Donc si elle change avant l'execute, la requête changera.
+    With bindValue, the value in the variable is passed directly to our prepared statement.
+    With bindParam, the entire variable is passed. So if it changes before execution, the statement will change.
 */
 
 $title = " Connexion à la Base de donnée ";
